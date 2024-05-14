e453c6099a2bcce5aa4d534e64260d6ed2200456
Chaten Mundle <chetanmundle7875@gmail.com>
2024-05-14 12:48:20 +0530

    tailwind

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,8 +6,8 @@ const About = () => {
   return (
     <div>
       <Navbar />
-      <div className="about-path">Home » About Sanathan » AboutUs</div>
-
+      <div className="about-path">Home &#9654; About Sanathan &#9654; AboutUs</div>
+<div className="font-bold bg-red-600">Chetan</div>
       <Footer />
     </div>
   );
