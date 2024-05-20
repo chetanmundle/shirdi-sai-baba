59548614bcedf0dc56fcfdf9b151d740c4f6aa77
Pratik_Odpalliwar <pratikodpalliwar777@gmail.com>
2024-05-20 11:36:34 +0530

    history page and dailyprogram page is updated

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { Route, Router, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import DailyProgram from "./pages/dailyprogram/DailyProgram";
+import History from "./pages/history/History";
 
 function App() {
   return (
@@ -16,6 +17,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About/>}/>
         <Route path="/dailyprogram" element={<DailyProgram/>}/>
+        <Route path="/history" element={<History/>}/>
+
       </Routes>
     </div>
   );
--- src/pages/dailyprogram/DailyProgram.jsx
+++ src/pages/dailyprogram/DailyProgram.jsx
@@ -3,70 +3,180 @@ import { delay, motion, useInView } from 'framer-motion';
 import "./dailyprogram.css"
 import { duration } from '@mui/material';
 import map from "../../assets/map.png"
+import Navbar from '../../components/Navbar';
+import Social from '../../components/social-icon/Social';
+import Footer from '../../components/Footer';
+import saiimg from "../../assets/images/saibaba4.jpg";
+
 
 const DailyProgram = () => {
-//   const experienceRef = useRef();
-//   const isExperienceRefInView = useInView(experienceRef, { margin: '-100px' });
+
 
   return (
-    <div className="container">
-      <motion.h1
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 1 }}
-        className="section-title"
-      >
-   Shedule
-      </motion.h1>
+    <>
+<div className="about-main">
+      <Navbar />
+      <Social />
+      <marquee behavior="" direction="">
+        🚩
+        <span className="about-marquee">
+          Shri Saibaba Sansthan Trust, Shirdi
+        </span>
+        🚩
+      </marquee>
+      <div className="about-path">
+        <span> Home &#9654; About Sanathan &#9654; AboutUs</span>
+      </div>
+
+      <div
+        className="about-background"
+        style={{ backgroundImage: `url(${saiimg})` }}
+      ></div>
+
+      <div className="about-head">
+        <h1>Daily Programme</h1>
+      </div>
+
+      <div className="container">
 
 
 <div className="sub-container">
 
       <motion.div
-        whileInView={{ opacity: 1, x: 0 }}
-        initial={{ opacity: 0, x: -100 }}
-        transition={{ duration: 1 }}
-        className=""
+       animate={{ opacity: 1, x: 0 }}
+            initial={{ opacity: 0, x: -100 }}
+            transition={{ duration: 2.5}}
+            className='img1'
         >
-
-
         <div className="img-container">
-          {/* <div className="img-details">
-            <div className="img-title">Heading</div>
-             </div> */}
         <img src={map} alt="" />
-
+        <div className="img-title">Heading</div>
         </div>
-
         </motion.div>
+{/* 
+        <motion.div
+       animate={{ opacity: 1, x: 0 }}
+            initial={{ opacity: 0, x: -100 }}
+            transition={{ duration: 2.5}}
+            className='img2'
+        >
+        <div className="img-container">
+        <img src={map} alt="" />
+        <div className="img-title">Heading</div>
+        </div>
+        </motion.div>
+
+        <motion.div
+       animate={{ opacity: 1, x: 0 }}
+            initial={{ opacity: 0, x: -100 }}
+            transition={{ duration: 2.5}}
+            className='img3'
+        >
+        <div className="img-container">
+        <img src={map} alt="" />
+        <div className="img-title">Heading</div>
+        </div>
+        </motion.div>
+
+        <motion.div
+       animate={{ opacity: 1, x: 0 }}
+            initial={{ opacity: 0, x: -100 }}
+            transition={{ duration: 2.5}}
+            className='img4'
+        >
+        <div className="img-container">
+        <img src={map} alt="" />
+        <div className="img-title">Heading</div>
+        </div>
+        </motion.div>
+
+        <motion.div
+       animate={{ opacity: 1, x: 0 }}
+            initial={{ opacity: 0, x: -100 }}
+            transition={{ duration: 2.5}}
+            className='img5'
+        >
+        <div className="img-container">
+        <img src={map} alt="" />
+        <div className="img-title">Heading</div>
+        </div>
+        </motion.div>
+
+        <motion.div
+       animate={{ opacity: 1, x: 0 }}
+            initial={{ opacity: 0, x: -100 }}
+            transition={{ duration: 2.5}}
+            className='img6'
+        >
+        <div className="img-container">
+        <img src={map} alt="" />
+        <div className="img-title">Heading</div>
+        </div>
+        </motion.div> */}
+
+
 
         <div className="line-container">
           <div className="line">
           <div className="line-circle"></div>
           </div>
-           <div className="line-two">
+           <div className="line2">
+          <div className="line-circle"></div>
+          </div>
+          <div className="line3">
+          <div className="line-circle"></div>
+          </div>
+           <div className="line4">
+          <div className="line-circle"></div>
+          </div>
+          <div className="line5">
+          <div className="line-circle"></div>
+          </div>
+           <div className="line6">
+          <div className="line-circle"></div>
+          </div>
+          <div className="line7">
+          <div className="line-circle"></div>
+          </div>
+          <div className="line8">
+          <div className="line-circle"></div>
+          </div>
+          <div className="line9">
+          <div className="line-circle"></div>
+          </div>
+          <div className="line10">
+          <div className="line-circle"></div>
+          </div>
+          <div className="line11">
+          <div className="line-circle"></div>
+          </div>
+          <div className="line12">
           <div className="line-circle"></div>
           </div>
           </div>
+         
+        
 
         <motion.div
-        whileInView={{ opacity: 1, x: 0 }}
-        initial={{ opacity: 0, x: 100 }}
-        transition={{ duration:1}}
+        animate={{ opacity: 1, x: 0 }}
+            initial={{ opacity: 0, x: 100 }}
+            transition={{ duration: 0.5 }}
         className=""
         >
         <div className="img-container-two">
-          {/* <div className="img-details">
-            <div className="img-title">Heading</div>
-             </div> */}
         <img src={map} alt="" />
-
+        <div className="img-title">Heading</div>
         </div>
         </motion.div>
         
 
     </div>
         </div>
+
+
+                  <Footer/>
+    </div>
+        </>
   );
 };
 
